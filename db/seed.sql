262bfa5fc71c84f997b89e0b744ba33692672929
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-05-12 20:39:10 -0700

    Started to add seed info

--- db/seed.sql
+++ db/seed.sql
@@ -11,14 +11,14 @@ INSERT INTO
 position (title, salary, department_id) 
 
 VALUES
-('',),
-('',),
-('',),
-('',),
-('',),
-('',),
-('',),
-('',);
+('Head of HR', 150000),
+('Sales Representative/Assistant to the regional manager', 100000),
+('Senior Accountant', 300000),
+('Warehouse Foreman', 60000),
+('Accountant', 150000),
+('Sales Representative', 80000),
+('HR Assistant', 90000),
+('Forklift Operator', 50000);
 
 INSERT INTO 
 employee (first_name, last_name, manager_id, position_id) 
